0e12213332cbb90131c0790501d8849a0a0771e5
Harm Aarts <harmaarts@gmail.com>
2017-03-27 20:05:39 +0200

    Reuse Entry population from trakt_list
    More information is better, subsequent plugins can then make better
    decisions.

--- flexget/plugins/list/trending_list.py
+++ flexget/plugins/list/trending_list.py
@@ -4,7 +4,7 @@ from flexget import plugin
 from flexget.event import event
 from flexget.entry import Entry
 from flexget.plugins.internal.api_trakt import get_session, get_api_url
-
+from flexget.plugins.list.trakt_list import generate_show_title, field_maps
 
 class TrendingList(object):
 
@@ -24,10 +24,13 @@ class TrendingList(object):
         return [self._api_result_to_entry(result, kind) for result in results]
 
     def _api_result_to_entry(self, result, kind):
-        key = kind[0:-1] # this is either 'serie' or 'movie'
-        url = get_api_url(kind, result[key]['ids']['trakt'])
+        entry = Entry()
 
-        return Entry(result[key]['title'], url)
+        key = kind[0:-1] # this is either 'show' or 'movie'
+        entry['url'] = 'https://trakt.tv/%s/%s' %(kind, result[key]['ids']['slug'])
+        entry.update_using_map(field_maps[key], result)
+
+        return entry
 
 
 @event('plugin.register')
--- flexget/tests/test_trending_list.py
+++ flexget/tests/test_trending_list.py
@@ -13,11 +13,11 @@ class TestTrendingSerieList(object):
     def test_trending_series(self, execute_task):
         task = execute_task('test_trending_list')
         assert len(task.entries) == 10
-        assert task.entries[0].get('title') == 'The Walking Dead'
-        assert task.entries[1].get('title') == 'Homeland'
+        assert task.entries[0].get('title') == 'The Walking Dead (2010)'
+        assert task.entries[1].get('title') == 'Homeland (2011)'
 
-        assert task.entries[0].get('url') == 'https://api.trakt.tv/shows/1393'
-        assert task.entries[1].get('url') == 'https://api.trakt.tv/shows/1398'
+        assert task.entries[0].get('url') == 'https://trakt.tv/shows/the-walking-dead'
+        assert task.entries[1].get('url') == 'https://trakt.tv/shows/homeland'
 
 class TestTrendingMovieList(object):
     """
@@ -36,8 +36,8 @@ class TestTrendingMovieList(object):
     def test_trending_movies(self, execute_task):
         task = execute_task('test_trending_list')
         assert len(task.entries) == 10
-        assert task.entries[0].get('title') == 'Moana'
-        assert task.entries[1].get('title') == 'Rogue One: A Star Wars Story'
+        assert task.entries[0].get('title') == 'Moana (2016)'
+        assert task.entries[1].get('title') == 'Rogue One: A Star Wars Story (2016)'
 
-        assert task.entries[0].get('url') == 'https://api.trakt.tv/movies/175475'
-        assert task.entries[1].get('url') == 'https://api.trakt.tv/movies/211396'
+        assert task.entries[0].get('url') == 'https://trakt.tv/movies/moana-2016'
+        assert task.entries[1].get('url') == 'https://trakt.tv/movies/rogue-one-a-star-wars-story-2016'
